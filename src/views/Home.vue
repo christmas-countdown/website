0db7af4706839ef2fe361c85040845861a635662
Isaac (eartharoid) <contact@eartharoid.me>
2020-10-26 23:50:46 +0000

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,30 @@
   <div class='content'>
     <Navbar view='Home' />
 
-	<mdb-container fluid>
+	<mdb-container> <!-- fluid -->
+		<mdb-row class='row'>
+			<mdb-col col='sm'>
+				<h1>Christmas Countdown</h1>
+			</mdb-col>
+		</mdb-row>
+
+		<mdb-row class='row'>
+			<mdb-col col='sm' class='box'>
+				<h3>Christmas is in</h3>
+				<h4 id='sleeps' class='number'>60</h4> <!--{{ sleeps }} or v-bind:-->
+				<h3>sleeps</h3>
+			</mdb-col>
+		</mdb-row>
+
+		<mdb-row class='row'>
+			<mdb-col sm='8' class='box'>
+				Live
+			</mdb-col>
+
+			<mdb-col sm='4' class='box'>
+				Total
+			</mdb-col>
+		</mdb-row>
 
 	</mdb-container>
   </div>
@@ -13,14 +36,38 @@
 import Navbar from '@/components/Navbar.vue';
 
 import {
-	mdbContainer
+	mdbContainer,
+	mdbRow,
+    mdbCol
 } from 'mdbvue';
 
 export default {
   name: 'Home',
   components: {
 	Navbar,
-	mdbContainer
+	mdbContainer,
+	mdbRow,
+    mdbCol
   }
 };
 </script>
+
+<style scoped>
+
+	h1 {
+		font-family: 'MerryChristmas';
+		font-size: 5rem;
+		text-shadow: 3px 3px 3px black;
+		/* margin: 1rem; */
+	}
+
+	.box {
+		background: rgba(0, 0, 0, 0.8);
+		padding: 5%;
+		border-radius: 15px;
+	}
+
+	.number {
+		font-family: 'Pacifico', cursive;
+	}
+</style>

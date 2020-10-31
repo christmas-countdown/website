53fc782fbf61c9bb3cc6b566489909eb90ecfd18
Isaac (eartharoid) <contact@eartharoid.me>
2020-10-31 23:55:20 +0000

    Update Discord.vue

--- src/views/Discord.vue
+++ src/views/Discord.vue
@@ -27,55 +27,109 @@
 		</div>
 
 		<mdb-row>
-			<mdb-col lg='7' class='box z-depth-5 hvr-grow left'>
+			<mdb-col lg='8' class='box z-depth-5 hvr-grow left'>
 				<mdb-row>
 					<mdb-col>
-						<h6>Server and user</h6>
-						<h4>timezones</h4>
+						<div class='header'>
+							<h6>Server and user</h6>
+							<h4>timezones</h4>
+						</div>
+						<p>
+							Make the commands more accurate by setting your user timezone. This will be used when you use a Christmas command on any server.
+						</p>
+						<p>
+							The server's timezone is used for the automatic countdown in the morning, and will be used as the default when a user who has not set a personal timezone uses a command.
+						</p>
 					</mdb-col>
 				</mdb-row>
 			</mdb-col>
 		</mdb-row>
 
 		<mdb-row>
-			<mdb-col lg='5'>
+			<mdb-col lg='4'>
 			</mdb-col>
 			<mdb-col class='box z-depth-5 hvr-grow right'>
 				<mdb-row>
 					<mdb-col>
-						<h6>Server and user</h6>
-						<h4>locales</h4>
+						<div class='header'>
+							<h6>Server and user</h6>
+							<h4>locales</h4>
+						</div>
+						<p>
+							Similarly to the timezones, users can set their own language, and administrators can set the server's default.
+						</p>
+						<p>
+							If the bot hasn't been translated to your language yet, you can contribute on <a href='https://github.com/eartharoid/ChristmasCountdownBot'>GitHub</a>.
+						</p>
 					</mdb-col>
 				</mdb-row>
 			</mdb-col>
 		</mdb-row>
 
 		<mdb-row>
-			<mdb-col lg='7' class='box z-depth-5 hvr-grow left'>
+			<mdb-col lg='8' class='box z-depth-5 hvr-grow left'>
 				<mdb-row>
 					<mdb-col>
+					<div class='header'>
 						<h6>Time left to Christmas in</h6>
 						<h4>sleeps? days? hours?</h4>
+					</div>
+						<p>
+							Get the number of weeks, sleeps, days, hours, minutes, seconds, or the total time left until Christmas.
+						</p>
 					</mdb-col>
 				</mdb-row>
 			</mdb-col>
 		</mdb-row>
 		
 		<mdb-row>
-			<mdb-col lg='5'>
+			<mdb-col lg='4'>
+				
 			</mdb-col>
 			<mdb-col class='box z-depth-5 hvr-grow right'>
 				<mdb-row>
 					<mdb-col>
-						<h4>Logical</h4>
-						<h6>commands</h6>
+						<div class='header'>
+							<h4>Logical</h4>
+							<h6>commands</h6>
+						</div>
+						<p>
+							The bot is easy to use and has a useful help command, with many links to the <a href='https://docs.christmascountdown.live'>documentation</a> where everything is explained in more detail.
+						</p>
 					</mdb-col>
 				</mdb-row>
 			</mdb-col>
 		</mdb-row>
-
 	</mdb-container>
 
+	<mdb-container>
+		<mdb-row>
+			<mdb-col col='md'>
+				<img src='https://i.imgur.com/hfF9C3k.png' class='rounded img-fluid zoom' />
+			</mdb-col>
+			<mdb-col col='md'>
+				<img src='https://i.imgur.com/oIXzBfV.png' class='rounded img-fluid zoom' />
+			</mdb-col>
+		</mdb-row>
+
+		<mdb-row>
+			<mdb-col col='md'>
+				<img src='https://i.imgur.com/TK9SW2u.png' class='rounded img-fluid zoom' />
+			</mdb-col>
+			<mdb-col col='md'>
+				<img src='https://i.imgur.com/CVDy45B.png' class='rounded img-fluid zoom' />
+			</mdb-col>
+		</mdb-row>
+
+		<div id='title' style='margin-top:3rem;'>
+			<p>
+				Why haven't you added it yet?
+			</p>
+			<a href='/invite'>
+				<mdb-btn style='background-color:#7289DA;color:white;'>Add to Discord</mdb-btn> <!-- color='primary' -->
+			</a>
+		</div>
+	</mdb-container>
 	<Footer />
   </div>
 </template>
@@ -129,8 +183,12 @@ export default {
 		/* padding: 5%; */
 		padding: 3%;
 		/* border-radius: 15px; */
-		margin: 0.7rem;
+		margin: 1rem 0 2rem 0;
 		box-shadow: 2px 3px 15px rgba(0, 0, 0, 0.8);
+	}
+
+	.header {
+		margin-bottom: 2rem;
 	}
 
 	.left {
@@ -140,6 +198,11 @@ export default {
 	.right {
 		text-align: right;
 		float: right;
+	}
+
+	.img-fluid {
+		box-shadow: 2px 3px 15px rgba(0, 0, 0, 0.8);
+		margin-top: 2rem;
 	}
 
 </style>

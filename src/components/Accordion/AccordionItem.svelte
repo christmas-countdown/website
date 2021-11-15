c2c3b8cbacd2c41626dd1d85cc3f6c4e62d47987
Isaac <git@eartharoid.me>
2021-11-15 22:52:02 +0000

    less annnoying

--- src/components/Accordion/AccordionItem.svelte
+++ src/components/Accordion/AccordionItem.svelte
@@ -14,10 +14,11 @@
 	class="backdrop-filter backdrop-blur-lg backdrop-saturate-150 bg-primary bg-opacity-20 rounded-lg p-2 px-4 shadow-xl border-primary border-opacity-40 border-2 hover:border-opacity-60 m-2"
 >
 	<div on:click={toggle} class="cursor-pointer">
-		<a href="#{prefix + id}" class="flex w-full">
-		<span class="font-mono text-primary font-semibold hover:underline">{name}</span>
-	</a>
-	<span class="block text-gray-400 font-semibold">{description || ''}</span>
+		<!-- <a href="#{prefix + id}" class="flex w-full">
+			<span class="font-mono text-primary font-semibold hover:underline">{name}</span>
+		</a> -->
+		<span class="flex w-full font-mono text-primary font-semibold hover:underline">{name}</span>
+		<span class="block text-gray-400 font-semibold">{description || ''}</span>
 	</div>
 	<div class={$open === id ? '' : 'hidden'}>
 		<slot />

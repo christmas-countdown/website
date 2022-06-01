b10e15908a9d53ce4d77f4555f3325de0d3beac7
Isaac <git@eartharoid.me>
2022-06-01 13:14:33 +0100

    Add redirect

--- src/routes/invite.svelte
+++ src/routes/invite.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-	const url = 'https://discord.com/oauth2/authorize?client_id=509851616216875019&permissions=537149440&scope=applications.commands bot';
+	const url = 'https://discord.com/api/oauth2/authorize?client_id=509851616216875019&permissions=537150464&scope=bot+applications.commands&response_type=code&redirect_uri=https%3A%2F%2Fchristmascountdown.live%2Fdiscord%2Fenable-the-countdown';
 	export const load = () => {
 		return {
 			status: 302,

70facad6e72381c4a7da50c0da58d8e4dcb7689d
Isaac <git@eartharoid.me>
2022-09-20 16:46:45 +0100

    Update add.svelte

--- src/routes/discord/add.svelte
+++ src/routes/discord/add.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-	const url = 'https://discord.com/api/oauth2/authorize?client_id=509851616216875019&permissions=537150464&scope=bot+applications.commands&response_type=code&redirect_uri=https%3A%2F%2Fchristmascountdown.live%2Fdiscord%2Fenable-the-countdown';
+	const url = 'https://discord.com/api/oauth2/authorize?client_id=509851616216875019&permissions=537150464&scope=bot+applications.commands&response_type=code&redirect_uri=https%3A%2F%2Fchristmascountdown.live%2Fcallback';
 	export const load = () => {
 		return {
 			status: 302,

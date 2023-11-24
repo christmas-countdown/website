90cca00dcfe93bc7a1bd3de1e6f2d7a319024c48
Isaac <git@eartharoid.me>
2023-11-24 02:21:56 +0000

    Fix jokes page

--- src/routes/api/joke/+server.js
+++ src/routes/api/joke/+server.js
@@ -12,6 +12,6 @@ export function GET({ url }) {
 	if (lang && !Object.keys(jokes).includes(lang)) throw error(400, `Error: "${lang}" is not a supported language. Must be one of: ${Object.keys(jokes).join()}`);
 	else lang = 'en';
 	const random = jokes[lang][Math.floor(Math.random() * jokes[lang].length)];
-	return json(JSON.stringify(random));
+	return json(random);
 }
 

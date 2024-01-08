3899363c56fb88f4614f5a6b239cee04610ad32c
Isaac <git@eartharoid.me>
2024-01-08 20:51:26 +0000

    Update and move to Cloudflare Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
-import { markdown } from 'svelte-preprocess-markdown';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	kit: {
+		// https://kit.svelte.dev/docs/adapter-cloudflare#notes
+		// _redirects doesn't work for SvelteKit/function-handled routes
 		adapter: adapter(),
 		alias: {
 			$components: './src/components',
@@ -14,11 +18,11 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte'
 	},
-	preprocess: [preprocess(), markdown({
-		gfm: true,
-		headerIds: true
-		// headerPrefix: 'jt:'
-	})]
+
+	preprocess: [
+		preprocess(),
+		mdsvex(mdsvexConfig)
+	]
 };
 
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('vite').UserConfig} */
-const config = { plugins: [sveltekit()] };
+const config = {
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA(),
+	]
+};
 
 export default config;
